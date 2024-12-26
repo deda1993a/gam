a84c7a7c6855dd576f8b6a4ea1903fbdf1b7a63f
Dávid Demeter <92177837+deda1993a@users.noreply.github.com>
2024-12-26 13:03:19 +0100

    Add files via upload

---
+++ new/PSGridTagHelper.cs
@@ -0,0 +1,403 @@
+﻿using Gamanet.Common.Web.Model;
+using Gamanet.Common.Web.Model.Interfaces;
+using Gamanet.PortalServices.Controls.Helper;
+using Gamanet.PortalServices.Controls.Model;
+using Gamanet.PortalServices.Controls.Model.Interfaces;
+using Gamanet.PortalServices.Controls.TagHelpers.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Localization;
+using System.Text;
+using System.Text.Json;
+
+namespace Gamanet.PortalServices.Controls.TagHelpers
+{
+    [HtmlTargetElement("psgrid")]
+    [RestrictChildren("psgridcolumn")]
+    public class PSGridTagHelper : TagHelper
+    {
+        private IStringLocalizer<SharedResource> _loc { get; }
+
+        [HtmlAttributeName("id")]
+        public string Id { get; set; } = string.Empty;
+
+        [HtmlAttributeName("datasource-url")]
+        public string DataEndPoint { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Used for calculation of Table Height
+        /// </summary>
+        [HtmlAttributeName("rows-per-page")]
+        public int RowsPerPage { get; set; } = -1;
+
+        [HtmlAttributeNotBound]
+        public List<IGridColumnEntity> Columns { get; set; } = default!;
+
+        /// <summary>
+        /// User for initial data loaded and delivered as part of HTML page
+        /// </summary>
+        [HtmlAttributeName("dataprovider")]
+        public object? DataSource { get; set; } = null;
+
+        [HtmlAttributeName("grid-parameters")]
+        public object? Data { get; set; } = null;
+
+        /// <summary>
+        /// Determines the value of toggle buttons used for filtering rows
+        /// Default value = null
+        /// </summary>
+        [HtmlAttributeName("filtervalues")]
+        public string[]? FilterValues { get; set; } = null;
+
+        /// Determines the text of toggle buttons used for filtering rows
+        /// Default value = null
+        [HtmlAttributeName("filtertextkeys")]
+        public string[]? FilterTextKeys { get; set; } = null;
+
+        /// <summary>
+        /// Determines which toggle button used for filtering should be selected by default based on button index.
+        /// Indexing starts at 1
+        /// Default value = 1
+        /// </summary>
+        [HtmlAttributeName("selectedfilterindex")]
+        public int SelectedFilterIndex { get; set; } = 1;
+
+        [HtmlAttributeNotBound]
+        private string? SerializedData { get; set; } = null;
+
+        [HtmlAttributeNotBound]
+        private PagerResponseEntity? DataSet { get; set; } = null;
+
+        /// <summary>
+        /// Initial sort field when grid is loaded calculated in ConsolidateSortFields();
+        /// </summary>
+        [HtmlAttributeNotBound]
+        private string SortField { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Initial sort order when grid is loaded calculated in ConsolidateSortFields();
+        /// </summary>
+        [HtmlAttributeNotBound]
+        private string SortOrder { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Initial flter text when grid is loaded calculated in DetermineFilterText();
+        /// </summary>
+        [HtmlAttributeNotBound]
+        private string FilterValue { get; set; } = string.Empty;
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext? ViewContext { get; set; }
+
+        private IUserInfoService _userInfoSvc;
+
+        public PSGridTagHelper(IStringLocalizer<SharedResource> localizer, IUserInfoService infoservice)
+        {
+            _loc = localizer;
+            Columns = new List<IGridColumnEntity>();
+            _userInfoSvc = infoservice;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.IsNullOrEmpty(Id) == true) throw new ArgumentNullException(nameof(Id));
+
+            if (FilterTextKeys?.Length != FilterValues?.Length)
+            {
+                throw new ArgumentException("The 'filtertextkeys' and 'filtervalues' attribute arrays should have the same length (same number of elements).");
+            }
+
+            context.Items.Add(typeof(PSGridTagHelper), this);
+            _ = await output.GetChildContentAsync();
+
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.Add("id", Id);
+            output.Attributes.Add("class", "psgrid");
+
+            ConsolidateSortFields();
+            DetermineCurrentFilterKey();
+            await GetInitialDataAsync();
+
+
+            output.Content.SetHtmlContent(CreateHtmlContent());
+
+            await base.ProcessAsync(context, output);
+        }
+
+        private async Task GetInitialDataAsync()
+        {
+            if (DataSource == null) return;
+            Guid? userId = null;
+
+            string? suserid = _userInfoSvc.GetUserSetting(ViewContext?.HttpContext, "userid");
+
+            if (Guid.TryParse(suserid, out Guid parsedUserId) == true) userId = parsedUserId;
+
+            if (Data != null) SerializedData = JsonSerializer.Serialize(Data, PSSerialization.GetDefaultOptions());
+
+            var request = new PagerRequestEntity()
+            {
+                page = 1,
+                sort_field = SortField,
+                sort_order = SortOrder,
+                filter_value = FilterValue,
+                user_id = userId,
+                data = SerializedData
+            };
+
+            if (DataSource is IPagerDataProvider)
+            {
+                DataSet = new PagerResponseService((IPagerDataProvider)DataSource).GetPage(request, RowsPerPage);
+            }
+            else if (DataSource is IPagerDataProviderAsync)
+            {
+                DataSet = await new PagerResponseServiceAsync((IPagerDataProviderAsync)DataSource).GetPageAsync(request, RowsPerPage);
+            }
+        }
+
+        private string CreateHtmlContent()
+        {
+            var content = new StringBuilder();
+            int i = 1;
+            int height = RowsPerPage * 40 + 48;
+
+            content.I(i).AppendLine($"");
+            content.I(i).AppendLine($"<div class=\"psgrid-top-container\">");
+
+
+            content.I(i + 1).AppendLine($"<div class=\"psgrid-filter-container\">");
+            if (FilterValues?.Length > 0)
+            {
+                CreateToggleButtons(content, i + 1);
+            }
+            content.I(i + 1).AppendLine("</div>");
+
+            content.I(i + 1).AppendLine($"<div class=\"psgrid-search-container\">");
+            content.I(i + 2).AppendLine($"<input type=\"text\" name=\"psgrid-search\" placeholder=\"{_loc["txtTypeAKeyword"]}\">");
+            content.I(i + 2).AppendLine($"<button name=\"psgrid-search-button\" class=\"psgrid-search-button\" type=\"submit\" data-isempty=\"false\"></button>");
+            content.I(i + 1).AppendLine($"</div>");
+            content.I(i).AppendLine($"</div>");
+            content.I(i).AppendLine($"");
+            content.I(i).AppendLine($"<div class=\"psgrid-container-table\" style=\"width: 100%; height: {height}px\">");
+            content.I(i + 1).AppendLine($"<table class=\"psgrid-table\">");
+            CreateHeader(content, i + 2);
+            content.I(i + 2).AppendLine($"<tbody class=\"psgrid-table-body\" name=\"psgrid-table-body\" data-pagerows=\"6\" ></tbody>");
+            content.I(i + 1).AppendLine($"</table>");
+            content.I(i).AppendLine($"</div>");
+            content.I(i).AppendLine($"");
+            CreatePager(content, i);
+            content.I(i).AppendLine($"");
+            CreateRowTemplate(content, i);
+            content.I(i).AppendLine($"");
+            CreateScript(content, i);
+
+            return content.ToString();
+        }
+
+        private void CreateToggleButtons(StringBuilder content, int indent)
+        {
+            if (FilterTextKeys == null) return;
+
+            int i = indent;
+
+            content.I(i).AppendLine("<div id=\"pstogglebutton\" class=\"pstogglebutton\">");
+
+            for (int j = 0; j < FilterValues?.Length; j++)
+            {
+                var dataSelected = SelectedFilterIndex == j + 1 ? "true" : "false";
+
+                content.I(i + 1).AppendLine($"<button class=\"pstogglebutton-button\" name=\"pstogglebutton-filter-button\" value=\"{FilterValues[j]}\" data-selected=\"{dataSelected}\">");
+                content.I(i + 2).AppendLine($"<div class=\"pstogglebutton-text\">{_loc[FilterTextKeys[j]]}</div>");
+                content.I(i + 1).AppendLine("</button>");
+            }
+            content.Append("\n");
+
+            content.I(i).AppendLine("</div>");
+        }
+
+        private void CreateHeader(StringBuilder content, int indent)
+        {
+            int i = indent;
+            content.I(i).AppendLine($"<thead class=\"psgrid-table-head\" name=\"psgrid-header\">");
+            content.I(i + 1).AppendLine($"<tr class=\"psgrid-thead-row\">");
+
+            foreach (var column in Columns)
+            {
+                // Conversion for Asc/Desc/None to up/down/none
+                var order = column.Sorted.ToString().ToLower();
+                var issortable = (column.IsSortable || column.Sorted != enOrderType.None) ? $"sortable=\"{order}\"" : "";
+
+                content.I(i + 2).AppendLine($"<th {issortable} data-field=\"{column.TextField}\">");
+                content.I(i + 3).Append($"<div>");
+                content.Append($"{_loc[column.Title]}");
+                content.Append($"<span class=\"psgrid-sort\"></span>");
+                content.AppendLine($"</div>");
+                content.I(i + 2).AppendLine($"</th>");
+            }
+
+            content.I(i + 1).AppendLine($" </tr>");
+            content.I(i).AppendLine($" </thead>");
+        }
+
+        private void CreatePager(StringBuilder content, int indent)
+        {
+            int i = indent;
+            // Pager is created by JavaScript, we have to create a placeholder for it
+            content.I(i).AppendLine($"<div class=\"psgrid-paginator\" name=\"psgrid-pager\"></div>");
+        }
+
+        private void CreateRowTemplate(StringBuilder content, int indent)
+        {
+            int i = indent;
+
+            content.I(i).AppendLine($"<template name=\"psgrid-row-template\">");
+            content.I(i + 1).AppendLine($"<tr class=\"psgrid-tbody-row\">");
+
+            foreach (var column in Columns)
+            {
+                content.I(i + 2).Append($"<td>");
+                if (string.IsNullOrEmpty(column.OnClick) == false)
+                    content.Append($"<div class=\"psgrid-column-onclick\" name=\"{column.OnClick}\" onclick=\"\" >");
+
+                if (string.IsNullOrEmpty(column.LinkField) == false)
+                    content.Append($"<a name=\"{column.LinkField}\" href=\"\" >");
+
+                if (string.IsNullOrEmpty(column.ImageField) == false)
+                    content.Append($"<img class=\"psgrid-image\" name=\"{column.ImageField}\" src=\"\">");
+
+                if (string.IsNullOrEmpty(column.TextField) == false)
+                    content.Append($"<span name=\"{column.TextField}\"></span>");
+
+                if (string.IsNullOrEmpty(column.LinkField) == false)
+                    content.Append($"</a>");
+
+                if (string.IsNullOrEmpty(column.OnClick) == false)
+                    content.Append($"</div>");
+
+                content.AppendLine($"</td>");
+            }
+
+            content.I(i + 1).AppendLine($"</tr>");
+            content.I(i).AppendLine($"</template>");
+        }
+
+        private void CreateScript(StringBuilder content, int indent)
+        {
+            int i = indent;
+            content.AppendLine("");
+            content.I(i).AppendLine($"<script type=\"text/javascript\">");
+            content.I(i + 1).AppendLine("$(document).ready(function () {");
+            content.I(i + 2).AppendLine($"$('#{Id}').initGrid({{");
+            content.I(i + 2).AppendLine($"dataEndPoint: '{DataEndPoint}',");
+            GetRowsInitialData(content, i + 2);
+            if (DataSet != null)
+            {
+                content.I(i + 2).AppendLine($"totalPages: {DataSet.pages_total},");
+                content.I(i + 2).AppendLine($"currentPage: {DataSet.page},");
+                content.I(i + 2).AppendLine($"totalRows: {DataSet.rows_total},");
+            }
+
+            content.I(i + 2).AppendLine($"selectedFilterIndex: '{SelectedFilterIndex}',");
+            content.I(i + 2).AppendLine($"sortField: '{SortField}',");
+            content.I(i + 2).AppendLine($"sortOrder: '{SortOrder}',");
+            content.I(i + 2).AppendLine($"gridParams: '{SerializedData}',");
+            GetBindDataFunction(content, i + 2);
+            content.I(i + 1).AppendLine("});");
+            content.AppendLine("");
+            content.I(i + 1).AppendLine($"$('#{Id}').loadData();");
+            content.I(i).AppendLine("});");
+            content.I(i).AppendLine("</script>");
+            content.AppendLine("");
+        }
+
+        private void GetRowsInitialData(StringBuilder content, int indent)
+        {
+            if (DataSet == null) return;
+
+            var jdata = JsonSerializer.Serialize(DataSet.data, PSSerialization.GetDefaultOptions());
+            content.I(indent).AppendLine($"rows: {jdata},");
+        }
+
+        private void GetBindDataFunction(StringBuilder content, int indent)
+        {
+            int i = indent;
+
+            content.I(i).AppendLine("bindData: function (data, tbody, rtemplate) {");
+            content.I(i + 1).AppendLine("$.each(data, function(index, row) {");
+            content.I(i + 2).AppendLine("const newRow = document.importNode(rtemplate, true);");
+
+
+            foreach (var column in Columns)
+            {
+                if (string.IsNullOrEmpty(column.TextField) == false)
+                    content.I(i + 2).AppendLine($"$(newRow).find('[name=\"{column.TextField}\"]').text({FormatField(column)});");
+
+                if (string.IsNullOrEmpty(column.ImageField) == false)
+                    content.I(i + 2).AppendLine($"$(newRow).find('[name=\"{column.ImageField}\"]').attr('src', row.{column.ImageField});");
+
+                if (string.IsNullOrEmpty(column.LinkField) == false)
+                    content.I(i + 2).AppendLine($"$(newRow).find('[name=\"{column.LinkField}\"]').attr('href', row.{column.LinkField});");
+
+                if (string.IsNullOrEmpty(column.OnClick) == false)
+                    content.I(i + 2).AppendLine($"$(newRow).find('[name=\"{column.OnClick}\"]').attr('onclick', row.{column.OnClick});");
+            }
+
+            content.I(i + 2).AppendLine("tbody.append(newRow);");
+            content.I(i + 1).AppendLine("});");
+            content.I(i).AppendLine("},");
+        }
+
+        private string FormatField(IGridColumnEntity? field)
+        {
+            if (field == null) return string.Empty;
+
+            string retval = string.Empty;
+            if (field.FieldType == Enums.enFieldType.Date)
+                //retval = $"new Date(row.{field.PageUrl}).toLocaleString('sk-sk')";
+                retval = $"getFormattedDate(row.{field.TextField})";
+            else
+                retval = $"row.{field.TextField}";
+
+
+            return retval;
+        }
+
+        private void ConsolidateSortFields()
+        {
+            bool isfirstsortedfield = true;
+            foreach (var column in Columns)
+            {
+                if (column.Sorted == enOrderType.None) continue;
+                if (isfirstsortedfield == false)
+                {
+                    column.Sorted = enOrderType.None;
+                    column.IsSortable = true;
+                    continue;
+                }
+
+                SortField = column.TextField;
+                SortOrder = column.Sorted.ToString().ToLower();
+                isfirstsortedfield = false;
+            }
+        }
+
+        private void DetermineCurrentFilterKey()
+        {
+            if (FilterValues == null)
+            {
+                return;
+            }
+
+            if (SelectedFilterIndex > FilterValues.Length || SelectedFilterIndex - 1 < 0)
+            {
+                throw new Exception("The SelectedFilterIndex is incorrect in relation of the filtervalues attribute");
+            }
+
+            FilterValue = FilterValues[SelectedFilterIndex - 1];
+        }
+
+    }
+}
---
+++ new/PSgrid.js
@@ -0,0 +1,288 @@
+﻿/** Functions Called from HTML  $(document).ready(function (){}) */
+var psGridInitiator = {
+    initGrid: function (config) {
+        var $psgrid = $.extend(psGrid, psGridPager, psGridSorter, psGridSearcher, psGridFilter);
+        $(this).data('psgrid', $psgrid);
+
+        $psgrid.config = config;
+        $psgrid.config.root = $(this);
+        $psgrid.initGrid();
+    },
+
+    loadData: function () {
+        var $psgrid = $(this).data('psgrid');
+        $psgrid.loadData();
+    }
+}
+
+$.fn.extend(psGridInitiator);
+
+/** Main Class for Gris Behavior Management **/
+var psGrid = {
+
+    config: {},
+    loadData: function () {
+        var $cfg = this.config
+        var $psgrid = this;
+
+        if ($cfg.isfirstload == true && isEmpty($cfg.rows, false) == false) {
+            this.loadRows();
+            return;
+        }
+
+        // load data from server
+        wsclient.GetParamApi($cfg.dataEndPoint,
+            { page: $cfg.currentPage, sort_field: $cfg.sortField, sort_order: $cfg.sortOrder, search_text: $cfg.searchText, filter_value: $cfg.filterValue, data: $cfg.gridParams },
+            function (result) {
+                $cfg.totalRows = result.rows_total;
+                $cfg.totalPages = result.pages_total;
+                $cfg.currentPage = result.page;
+                $cfg.rows = result.data;
+                $psgrid.loadRows();      
+            }
+        );
+
+    },
+    /** Internal function trigerred from wsclient callback */
+    loadRows: function () {
+        var $cfg = this.config
+
+        $cfg.isfirstload = false;
+        // clean rows in table
+        $cfg.tablebody.empty();
+        // generate rows in bindData function
+        $cfg.bindData($cfg.rows, $cfg.tablebody, $cfg.rowtemplate);
+
+        this.createPager();
+    },
+
+    /** Save configuration data defined in HTML page $(document).ready(function ()) to the psgrid element as custom data attribute
+     this is called from psgrid root element */
+    initGrid: function () {
+        $cfg = this.config;
+        $cfg.tablebody = $cfg.root.find('[name="psgrid-table-body"]');
+        $cfg.rowtemplate = $cfg.root.find('[name="psgrid-row-template"]').prop('content');
+        $cfg.pager = $cfg.root.find('[name="psgrid-pager"]');
+        $cfg.sorter = $cfg.root.find('[name="psgrid-header"]');
+        $cfg.searcher = $cfg.root.find('[name="psgrid-search"]');
+        $cfg.searchbtn = $cfg.root.find('[name="psgrid-search-button"]');
+        $cfg.filterbtns = $cfg.root.find('[name="pstogglebutton-filter-button"]');
+        $cfg.isfirstload = true;
+
+        this.createSorter();
+        this.createSearcher();
+        this.createFilter();
+    },
+};
+
+var psGridSearcher = {
+
+    /** Triggered by search text changed */
+    onSearchTextChanged: function (e) {
+        var $cfg = this.config;
+        $cfg.searchText = $cfg.searcher.val();
+        $cfg.searchbtn.attr('data-isempty', isEmpty($cfg.searchText) ? "false" : "true");
+    },
+    /** Triggered by search text changed */
+    onclickSearchButton: function () {
+        var $cfg = this.config;
+        $cfg.searcher.focus();
+        if (isEmpty($cfg.searcher.val()) == true) return;
+
+        $cfg.searcher.val('');
+        this.onSearchTextChanged();
+        this.loadData();
+    },
+    /** Triggered when Enter pressed */
+    onSearchEnter: function (e) {
+        if (e.keyCode !== 13) return;
+        this.loadData();
+    },
+    /** Attach keypress and data changed handler */
+    createSearcher: function () {
+        var $cfg = this.config;
+        var $psgrid = this;
+
+        $cfg.searcher.on('input', function (e) { $psgrid.onSearchTextChanged(e) });
+        $cfg.searcher.on('keydown', function (e) { $psgrid.onSearchEnter(e) });
+        $cfg.searchbtn.click(function () { $psgrid.onclickSearchButton(); });
+    },
+};
+
+var psGridFilter = {
+    onclickFilterButton: function (i) {
+        var $cfg = this.config;
+        var $filterbtns = $cfg.filterbtns;
+
+        var newFilterValue = $($filterbtns[i]).attr("value");
+        if (newFilterValue == $cfg.filterValue) {
+            return;
+        }
+
+        $cfg.filterValue= newFilterValue;
+        this.loadData();
+    },
+
+    createFilter: function () {
+        var $cfg = this.config;
+        var $psgrid = this;
+        var $filterbtns = $cfg.filterbtns;
+
+        $('#pstogglebutton').initToggleButton()
+
+        var selectedFilterIndex = parseInt($cfg.selectedFilterIndex) - 1;
+        $cfg.filterValue = $($filterbtns[selectedFilterIndex]).attr("value");
+
+        for (let i = 0; i < $filterbtns.length; i++) {
+            $($filterbtns[i]).click(function () { $psgrid.onclickFilterButton(i)})
+        }
+    }
+}
+
+var psGridSorter = {
+    /**Triggered by column header click */
+    onclickSort: function (column) {
+        var $cfg = this.config;
+        const $cols = $cfg.sorter.find('[sortable]');
+        $.each($cols, function (index, col) {
+            var $colstatus = $(col).attr("sortable");
+            var $colfield = $(col).data('field');
+
+            // skip if column is not clicked one
+            if ($colfield !== column) { $(col).attr("sortable", 'none'); return; }
+
+            // toggle up and down status for clicked column
+            var $order = ($colstatus === 'asc') ? 'desc' : 'asc';
+            $(col).attr("sortable", $order);
+
+            // Attach to config info and convert up to asc and down to desc
+            $cfg.sortField = $colfield;
+            $cfg.sortOrder = $order;
+            $cfg.root.loadData();
+        });
+    },
+    /** This is called from psgrid root element
+    initialize event handler for sortable header columns */
+    createSorter: function () {
+        var $cfg = this.config;
+        var $psgrid = this;
+
+        const $cols = $cfg.sorter.find('[sortable]');
+        $.each($cols, function (index, col) {
+            $(col).click(function () { var colname = $(this).data('field'); $psgrid.onclickSort(colname) });
+        });
+    },
+};
+
+var psGridPager = {
+
+    /** Triggered by pager button click.  
+     Handle page navigation to keep currentPage pageidx in valid range */
+    onClickPage: function (pageidx) {
+        var $cfg = this.config;
+        if (pageidx === $cfg.currentPage) return;
+
+        $cfg.currentPage = pageidx;
+        // if page changed, load data and update pager
+        this.selectPage(pageidx);
+        this.loadData();
+    },
+    /** Calculate page number when previous or next button clicked */
+    onCalculatePage: function (pageidx) {
+        var $cfg = this.config;
+        // Get current page from config
+        var $curpage = $cfg.currentPage;
+
+        switch (pageidx) {
+            case 0: // previous page
+                $curpage--;
+                break;
+            case -1: // next page
+                $curpage++;
+                break;
+            default:
+                $curpage = pageidx;
+                break;
+        }
+
+        // Validate is page number in valid range
+        if ($curpage < 1) $curpage = 1;
+        if ($curpage > $cfg.totalPages) $curpage = $cfg.totalPages;
+
+        this.onClickPage($curpage);
+    },
+    /** Set correct style for active page button and load data */
+    selectPage: function () {
+        var $cfg = this.config;
+        // Get current page from config
+        var $curpage = $cfg.currentPage;
+
+        // find page buttons (contains [data-index] attribute)
+        const $pages = $cfg.pager.find('[data-index]');
+        $.each($pages, function (index, row) {
+            var $pageidx = $(row).data("index");
+            var $status = ($pageidx === $curpage) ? 'active' : 'none';
+            $(row).attr("status", $status);
+        });
+
+    },
+    /** Adapt pager in relation to config totalPages after Data loaded */
+    createPager: function () {
+        var $cfg = this.config;
+        var $psgrid = this;
+
+        $cfg.pager.empty();
+        $chunks = this.chunckCalculator();
+
+        if (isEmpty($chunks) == true || $chunks[0].end == 1 || $chunks[0].end == 0) return;
+
+        $($cfg.pager).addClass("psgrid-paginator-not-empty");
+
+        var $prev = $('<div>').attr('status', 'none').click(function () { $psgrid.onCalculatePage(0); });
+        $prev.append($('<img>'));
+        $cfg.pager.append($prev);
+
+        $.each($chunks, function (index, chunk) {
+            for (var i = chunk.start; i <= chunk.end; i++) {
+                var $page = $('<div>').attr('status', 'none').attr('data-index', i).text(i).click(function () { var index = $(this).data('index'); $psgrid.onClickPage(index); });
+                $cfg.pager.append($page);
+            }
+
+            if($chunks.length === index + 1) return;
+            var $page = $('<div>').attr('status', 'none').text('...');
+            $cfg.pager.append($page);
+        });
+
+        var $next = $('<div>').attr('status', 'none').click(function () { $psgrid.onCalculatePage(-1); });
+        $next.append($('<img>'));
+        $cfg.pager.append($next);
+
+        this.selectPage();
+    },
+    chunckCalculator: function () {
+        var $cfg = this.config;
+
+        if (isEmpty($cfg.totalRows) == true) return [];
+
+        var $maxpages = 10;
+
+        // Full range is visible
+        if ($cfg.totalPages <= $maxpages) return [{ start: 1, end: $cfg.totalPages }];
+
+        var chunks = [];
+
+        var $chunk1 = ($cfg.currentPage <= ($maxpages - 3)) ? { start: 1, end: ($maxpages - 3) } : { start: 1, end: 2 };
+        chunks.push($chunk1);
+
+        if ($cfg.currentPage > ($maxpages - 3) && $cfg.currentPage <= ($cfg.totalPages - ($maxpages - 3))) {
+            var $chunk2 = { start: $cfg.currentPage - 1, end: $cfg.currentPage + 2 };
+            chunks.push($chunk2);
+        };
+
+        var $chunk3 = ($cfg.currentPage > ($cfg.totalPages - ($maxpages - 3))) ? { start: ($cfg.totalPages - ($maxpages - 4)), end: $cfg.totalPages, islastchunck: true } : { start: $cfg.totalPages - 1, end: $cfg.totalPages, islastchunk: true };
+        chunks.push($chunk3);
+
+        return chunks;
+    },
+};
+
